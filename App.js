d06004d4f77dd22075dcde63e4fe4956d5b6e794
Lisa Weilguni <lisa.weilguni@lingvano.com>
2023-07-31 11:05:01 +0200

    Add valid test case

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>What's up?</Text>
+      <Text>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
