59674f0f94fd4ad2510eee2678cb1905b6dbd7ad
Lisa Weilguni <lisa.weilguni@lingvano.com>
2023-07-31 12:14:21 +0200

    Try: use playwright/msw to intercept request

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,23 @@
-import { test, expect } from '@playwright/test';
-import { worker } from '../mocks/browser';
+import { expect, test } from './test';
+import { rest } from 'msw';
+
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:19006/');
 
   await expect(page.getByText('Mock Response')).toBeVisible();
+});
+
+test('can intercept request', async ({ page, worker }) => {
+  await worker.use(
+    rest.get('/api', (_, response, context) =>
+      response(
+        context.status(200),
+        context.json({ title: 'Intercepted Response' })
+      )
+    )
+  );
+
+  await page.goto('http://localhost:19006/');
+
+  await expect(page.getByText('Intercepted Response')).toBeVisible();
 });
